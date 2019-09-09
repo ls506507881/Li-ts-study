326020a13744ff348699821e9329baf095c84ab4
li_shuai <506507881@qq.com>
2019-09-09 22:15:17 +0800

    新建greeter.html

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,19 @@
-function greeter(person:string) {
-  return "Hello, " + person;
+class Student {
+  全名: string;
+  constructor(public 姓, public 名) {
+    this.全名 = 姓 + 名;
+  }
 }
 
-let user = "Herbert Lee";
+interface Person {
+  姓: string;
+  名: string;
+}
+
+function greeter(person : Student) {
+  return "Hello, " + person.全名;
+}
+
+let user = new Student("李", "帅帅");
 
 console.log(greeter(user))
