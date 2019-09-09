4890e7c1cb8c697a12d38543834c0619bc0d507f
li_shuai <506507881@qq.com>
2019-09-09 23:19:40 +0800

    min.ts 边写边查文档

---
+++ min.ts
@@ -0,0 +1,59 @@
+// function min(a:number,b:number):number{
+//   if(a<b){
+//     return a
+//   }else{
+//     return b
+//   }
+// }
+
+// var c = min(1,2)
+// console.log(c)
+
+// --------------
+
+// function count(a:string,b:string):string
+// function count(a:number,b:number):number
+// function count(a:any,b:any):any{
+//   return a + b
+// }
+// var b = count(5,7)
+// var c = count('5','7')
+// console.log(b)
+// console.log(c)
+
+// --------------
+
+// function selectSort(a:number[]):number[]{
+//   for(let i = 0;i<a.length-1;i++){
+//     let minIndex = i;
+//     for(let j=i+1;j<a.length;j++){
+//       if(a[j]<a[minIndex]){
+//         minIndex = j
+//       }
+//     }
+//     let temp = a[minIndex]
+//     a[minIndex] = a[i]
+//     a[i] = temp
+//   }
+//   return a
+// }
+
+// let b = selectSort([5,4,2,1,3])
+// console.log(b)
+
+// --------------
+interface Person{
+  gender: string;
+}
+
+function merry(a:Person,b:Person):[Person,Person]{
+  if(a.gender !== b.gender){
+    return [a,b];
+  }else{
+    throw new Error('性别相同不能结婚')
+  }
+}
+
+let a = { gender: '男' };
+let b = { gender:'女' };
+console.log(merry(a,b));
